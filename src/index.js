d6866a0b9d409dec51238c15e77c23e7b290b101
snapkov <snapkov@gmail.com>
2022-01-11 14:16:25 +0300

    feat: add function decode morse to text

--- src/index.js
+++ src/index.js
@@ -38,6 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    let result = "";
+    let char = "";
+    for (let i = 0; i < expr.length; i += 10) {
+        char = expr.slice(i, i + 10);
+        if (char === "**********") {
+            result += " ";
+            continue;
+        }
+        let decodeChar = "";
+        for (let j = 0; j < char.length; j += 2) {
+            switch (char.slice(j, j + 2)) {
+                case "10": 
+                    decodeChar += ".";
+                    break;
+                case "11": 
+                    decodeChar += "-";
+                    break;
+            }
+        }
+        result += MORSE_TABLE[decodeChar];
+    }
+    return result;
     // write your solution here
 }
 
